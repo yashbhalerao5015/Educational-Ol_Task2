492dc10e785e698845bcf147fbbbf36966915d15
yashbhalerao5015@gmail.com <yashbhalerao5015@gmail.com>
2021-08-01 13:57:18 +0530

    Intial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//alert("Hello World");
+AOS.init();
+
+function colorChanger(x) {
+  x.style.backgroundColor = "blue";
+  x.style.color = "white";
+
+
+
+
+}
+
+function revert() {
+  $(".card-1").css("background-color", "white");
+  $(".card-1").css("color", "black");
+  //$(".first").addClass("re-transorm");
+}
+
+function functionName() {
+  $(".list-group-item").css("background-color", "blue");
+}
